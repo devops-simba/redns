bb04d62589cf74ddc4c8ecc69f233e44a44e1a37
mohammadmahdi.roozitalab <mohammadmahdi.roozitalab@snapp.cab>
2020-10-01 16:57:41 +0330

    Fixed redns server with new definitions

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"strings"
 
 	"github.com/miekg/dns"
@@ -86,10 +87,12 @@ func (this *DNSServer) ServeDNS(w dns.ResponseWriter, msg *dns.Msg) {
 	if len(m.Answer) == 0 {
 		m.Rcode = dns.RcodeNameError
 		m.Answer = append(m.Answer, SOA(".", &definitions.DNSRecord{
-			NSRecords: &definitions.DNS_STRING_Record{
-				Addresses: []definitions.DNS_STRING_Address{
-					definitions.DNS_STRING_Address{
-						Value: "dns.cloud.snapp.ir",
+			NSRecords: &definitions.DNS_NS_Record{
+				Addresses: []definitions.DNS_NS_Address{
+					definitions.DNS_NS_Address{
+						DNS_STR_Address: definitions.DNS_STR_Address{
+							Value: "dns.cloud.snapp.ir",
+						},
 					},
 				},
 			},
@@ -104,117 +107,94 @@ func (this *DNSServer) ServeDNS(w dns.ResponseWriter, msg *dns.Msg) {
 func (this *DNSServer) Start() error    { return this.server.ListenAndServe() }
 func (this *DNSServer) Shutdown() error { return this.server.Shutdown() }
 
+func WeightedSelect(rec definitions.IDNSAddressRecord) definitions.IDNSAddress {
+	if rec.IsEmpty() {
+		panic("This function should only called on non-empty records")
+	}
+
+	addresses := rec.AddressList()
+	if len(addresses) == 1 {
+		return addresses[0]
+	}
+
+	overallWeight := 0
+	for i := 0; i < len(addresses); i++ {
+		overallWeight += int(addresses[i].BaseAddress().Weight)
+	}
+
+	n := rand.Int31n(int32(overallWeight))
+
+	index := 0
+	for i := 0; i < len(addresses); i++ {
+		weight := int32(addresses[i].BaseAddress().Weight)
+		if n < weight {
+			index = i
+			break
+		}
+
+		n -= weight
+	}
+
+	return addresses[index]
+}
+func ToRR(name string, rec definitions.IDNSAddressRecord, rec2 definitions.IDNSAddressRecord) []dns.RR {
+	activeRec := rec.LimitToActive()
+	if activeRec.IsEmpty() {
+		if rec2 != nil {
+			activeRec := rec2.LimitToActive()
+			if activeRec.IsEmpty() {
+				return nil
+			}
+		} else {
+			return nil
+		}
+	}
+
+	if activeRec.IsWeighted() {
+		return []dns.RR{WeightedSelect(activeRec).ToRR(name)}
+	}
+
+	return activeRec.ToRRList(name)
+}
+
 func A(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.ARecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		if !record.CNameRecords.IsEmpty() {
-			return CNAME(name, record)
-		}
-		return nil
-	}
-
-	if validAddresses.Weighted && validAddresses.Length() > 1 {
-		// find the record that we should return to the caller
-		return []dns.RR{
-			validAddresses.WeightedSelectAddress().ToA(name),
-		}
-	}
-
-	return validAddresses.ToAList(name)
+	return ToRR(name, record.ARecords, record.CNameRecords)
 }
 func AAAA(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.AAAARecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		if !record.CNameRecords.IsEmpty() {
-			return CNAME(name, record)
-		}
-		return nil
-	}
-
-	if validAddresses.Weighted && validAddresses.Length() > 1 {
-		// find the record that we should return to the caller
-		return []dns.RR{
-			validAddresses.WeightedSelectAddress().ToAAAA(name),
-		}
-	}
-
-	return validAddresses.ToAAAAList(name)
+	return ToRR(name, record.AAAARecords, record.CNameRecords)
 }
 func CNAME(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.CNameRecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		return nil
-	}
-
-	if validAddresses.Weighted && validAddresses.Length() > 1 {
-		// find the record that we should return to the caller
-		return []dns.RR{
-			validAddresses.WeightedSelectAddress().ToCNAME(name),
-		}
-	}
-
-	return validAddresses.ToCNAMEList(name)
+	return ToRR(name, record.CNameRecords, nil)
 }
 func NS(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.NSRecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		return nil
-	}
-
-	if validAddresses.Weighted && validAddresses.Length() > 1 {
-		// find the record that we should return to the caller
-		return []dns.RR{
-			validAddresses.WeightedSelectAddress().ToNS(name),
-		}
-	}
-
-	return validAddresses.ToNSList(name)
+	return ToRR(name, record.NSRecords, nil)
 }
 func TXT(name string, record *definitions.DNSRecord) []dns.RR {
-	if record.TXTRecords.IsEmpty() {
-		return nil
-	}
-
-	return record.TXTRecords.ToTXTList(name)
+	return ToRR(name, record.TXTRecords, nil)
 }
 func MX(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.MXRecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		return nil
-	}
-	if validAddresses.Weighted && validAddresses.Length() > 1 {
-		// find the record that we should return to the caller
-		return []dns.RR{
-			validAddresses.WeightedSelectAddress().ToMX(name),
-		}
-	}
-
-	return validAddresses.ToMXList(name)
+	return ToRR(name, record.MXRecords, nil)
 }
 func SRV(name string, record *definitions.DNSRecord) []dns.RR {
-	validAddresses := record.SRVRecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
-		return nil
-	}
-	return validAddresses.ToSRVList(name)
+	return ToRR(name, record.SRVRecords, nil)
 }
 
 func SOA(name string, record *definitions.DNSRecord, serialNumber uint32) []dns.RR {
-	validAddresses := record.NSRecords.FindValidAddresses()
-	if validAddresses.IsEmpty() {
+	nsRecord := record.NSRecords.LimitToActive()
+	if nsRecord.IsEmpty() {
 		return nil
 	}
 
 	mbox := ""
-	validMailAddresses := record.MXRecords.FindValidAddresses()
-	if !validMailAddresses.IsEmpty() {
-		mbox = validMailAddresses.Addresses[0].Value
+	mxRecord := record.MXRecords.LimitToActive()
+	if !mxRecord.IsEmpty() {
+		mbox = mxRecord.AddressList()[0].GetValue()
 	}
 
 	return []dns.RR{
 		&dns.SOA{
 			Hdr:     dns.RR_Header{Name: name, Class: dns.ClassINET, Rrtype: dns.TypeSOA, Ttl: 60},
-			Ns:      dns.Fqdn(validAddresses.Addresses[0].Value),
+			Ns:      dns.Fqdn(nsRecord.AddressList()[0].GetValue()),
 			Mbox:    mbox,
 			Serial:  serialNumber,
 			Refresh: 86400,
